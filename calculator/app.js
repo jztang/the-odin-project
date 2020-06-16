52faf4cef19681d0b9d74b571ecc592ceb4ec511
jztang <jztang8@gmail.com>
2020-06-16 12:49:00 -0400

    Add functionality for operators

--- calculator/app.js
+++ calculator/app.js
@@ -1,10 +1,13 @@
 let calcEntry = "";
 let hasDecimal = false;
+let isNumber = false;
+let isNegative = false;
 
 // numbers
 for (let i = 0; i <= 9; i++) {
     document.querySelector(`#button-${i}`).addEventListener("click", () => {
         calcEntry += i;
+        isNumber = true;
         updateDisplay();
     });
 }
@@ -14,9 +17,30 @@ document.querySelector("#button-decimal").addEventListener("click", () => {
     if (!hasDecimal) {
         calcEntry += ".";
         hasDecimal = true;
+        isNegative = true;
         updateDisplay();
     }
 });
+
+// operators
+let operators = ["divide", "multiply", "subtract", "add"];
+let operatorSymbols = ["÷", "×", "−", "+"];
+for (let i = 0; i < operators.length; i++) {
+    document.querySelector(`#button-${operators[i]}`).addEventListener("click", () => {
+        if (isNumber) {
+            calcEntry += operatorSymbols[i];
+            hasDecimal = false;
+            isNumber = false;
+            isNegative = false;
+            updateDisplay();
+        // subtract can also make a number negative
+        } else if (operators[i] === "subtract" && !isNegative) {
+            calcEntry += "-";
+            isNegative = true;
+            updateDisplay();
+        }
+    });
+}
 
 function operate(operator, x, y) {
     switch (operator) {
