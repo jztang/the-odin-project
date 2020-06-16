25b27183f92cb9b31f4b6f263054f419dec55ac2
jztang <jztang8@gmail.com>
2020-06-16 11:51:32 -0400

    Compress math functions

--- calculator/app.js
+++ calculator/app.js
@@ -1,38 +1,22 @@
-let displayText = "";
+let calcEntry = "";
 
 // numbers
 for(let i = 0; i <= 9; i++) {
     document.querySelector(`#button-${i}`).addEventListener("click", () => {
-        displayText += i;
+        calcEntry += i;
         updateDisplay();
     });
 }
 
-function add(x, y) {
-    return x + y;
-}
-
-function subtract(x, y) {
-    return x - y;
-}
-
-function multiply(x, y) {
-    return x * y;
-}
-
-function divide(x, y) {
-    return x / y;
-}
-
 function operate(operator, x, y) {
     switch (operator) {
-        case "add": return add(x, y); break;
-        case "subtract": return subtract(x, y); break;
-        case "multiply": return multiply(x, y); break;
-        case "divide": return divide(x, y); break;
+        case "add": return x + y;
+        case "subtract": return x - y;
+        case "multiply": return x * y;
+        case "divide": return x / y;
     }
 }
 
 function updateDisplay() {
-    document.querySelector("#display-text").textContent = displayText;
+    document.querySelector("#display-text").textContent = calcEntry;
 }
