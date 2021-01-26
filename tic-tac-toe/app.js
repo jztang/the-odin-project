0a8fa4715f0af75534a111529d32830633da0f5c
jztang <jztang8@gmail.com>
2021-01-26 01:05:54 -0500

    Render the contents of the game board

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,6 +1,21 @@
 const gameBoard = (() => {
     let board = new Array(9);
+    board = ["X", "O", "X", "", "", "", "O", "X", "O"];
     const getBoard = () => board;
 
     return { getBoard };
 })();
+
+const displayController = (() => {
+    const updateBoard = () => {
+        const squares = document.querySelector("#game-board").children;
+        const board = gameBoard.getBoard();
+        for(let i = 0; i < squares.length; i++) {
+            squares[i].textContent = board[i];
+        }
+    }
+
+    return { updateBoard }
+})();
+
+displayController.updateBoard();
