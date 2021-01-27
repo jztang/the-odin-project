bcf9b7ddf122f26b007118cd6f2ae32d7db13120
jztang <jztang8@gmail.com>
2021-01-26 19:17:35 -0500

    Update the game board when a player moves

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,21 +1,50 @@
 const gameBoard = (() => {
     let board = new Array(9);
-    board = ["X", "O", "X", "", "", "", "O", "X", "O"];
+    // board = ["X", "O", "X", "", "", "", "O", "X", "O"];
     const getBoard = () => board;
+    const setSquare = (index, marker) => { board[index] = marker; }
+    const resetBoard = () => { board = new Array(9); }
 
-    return { getBoard };
+    return { getBoard, setSquare, resetBoard };
 })();
 
 const displayController = (() => {
+    const squares = document.querySelector("#game-board").children;
+    
     const updateBoard = () => {
-        const squares = document.querySelector("#game-board").children;
         const board = gameBoard.getBoard();
-        for(let i = 0; i < squares.length; i++) {
+        for (let i = 0; i < squares.length; i++) {
             squares[i].textContent = board[i];
         }
     }
 
-    return { updateBoard }
+    const resetBoard = () => {
+        for (let i = 0; i < squares.length; i++) {
+            const curSquare = squares[i];
+            if (!curSquare.classList.contains("open")) {
+                curSquare.textContent = undefined;
+                curSquare.classList.add("open");
+                curSquare.addEventListener("click", () => {
+                    gameController.playerMove(i);
+                });
+            }
+        }
+    }
+    
+    return { updateBoard, resetBoard }
 })();
 
-displayController.updateBoard();
+const gameController = (() => {
+    let xToMove = true;
+
+    const playerMove = index => {
+        const moveMarker = xToMove ? "X" : "O";
+        gameBoard.setSquare(index, moveMarker);
+        xToMove = !xToMove;
+        displayController.updateBoard();
+    }
+
+    return { playerMove };
+})();
+
+displayController.resetBoard();
