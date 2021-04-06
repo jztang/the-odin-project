ec079d31c1c08c1dc02f0bbb7e04151e1e00f2a5
jztang <jztang8@gmail.com>
2021-04-06 16:56:57 -0400

    Check for a winner after every move, or a tie at the end

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -19,6 +19,10 @@ const displayController = (() => {
         }
     }
 
+    const setInfoText = text => {
+        document.querySelector("#info-text").textContent = text;
+    }
+
     const setUpBoard = () => {
         for (let i = 0; i < squares.length; i++) {
             squares[i].classList.add("open");
@@ -28,15 +32,26 @@ const displayController = (() => {
         console.log("First time setup done");
     }
 
-    const setInfoText = text => {
-        document.querySelector("#info-text").textContent = text;
+    const endGame = (indices, marker) => {
+        if (indices) {
+            setInfoText(`${marker} wins!`);
+            console.log(`Game over - ${marker} got 3-in-a-row: ${indices}`);
+        } else {
+            setInfoText("It's a tie!");
+            console.log("Game over - tie");
+        }
+
+        for (let i = 0; i < squares.length; i++) {
+            squares[i].classList.remove("open");
+        }
     }
 
-    return { getSquares, updateBoard, setUpBoard, setInfoText }
+    return { getSquares, updateBoard, setInfoText, setUpBoard, endGame }
 })();
 
 const gameController = (() => {
     let xToMove = true;
+    let movesMade = 0;
 
     const playerMove = event => {
         if (event.target.classList.contains("open")) {
@@ -56,11 +71,59 @@ const gameController = (() => {
             displayController.getSquares()[index].classList.remove("open");
             displayController.updateBoard();
             xToMove = !xToMove;
+            movesMade++;
             console.log(`${moveMarker} plays at square ${index}`);
+            checkForWin(moveMarker, index);
         }
     }
 
-    return { playerMove };
+    const checkForWin = (moveMarker, index) => {
+        const board = gameBoard.getBoard();
+
+        // check horizontal
+        const rowStart = Math.floor(index / 3) * 3;
+        const rowIndices = [rowStart, rowStart + 1, rowStart + 2];
+        if (checkSquares(rowIndices, moveMarker, board)) {
+            displayController.endGame(rowIndices, moveMarker);
+        }
+
+        // check vertical
+        const colStart = index % 3;
+        const colIndices = [colStart, colStart + 3, colStart + 6];
+        if (checkSquares(colIndices, moveMarker, board)) {
+            displayController.endGame(colIndices, moveMarker);
+        }
+
+        // check main diagonal
+        const mainDiaIndices = [0, 4, 8];
+        if (checkSquares(mainDiaIndices, moveMarker, board)) {
+            displayController.endGame(mainDiaIndices, moveMarker);
+        }
+
+        // check anti diagonal
+        const antiDiaIndices = [2, 4, 6];
+        if (checkSquares(antiDiaIndices, moveMarker, board)) {
+            displayController.endGame(antiDiaIndices, moveMarker);
+        }
+
+        // check for tie
+        if (movesMade === 9) {
+            displayController.endGame();
+        }
+    }
+
+    const checkSquares = (indices, marker, board) => {
+        let numInARow = 0;
+
+        indices.forEach(index => {
+            if (board[index] === marker) { numInARow++; }
+        });
+
+        if (numInARow === 3) { return true; }
+        return false;
+    }
+
+    return { playerMove, checkForWin };
 })();
 
 displayController.setUpBoard();
