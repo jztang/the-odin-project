a4a22e2e9430fa92e63abd3c92dd34e332847950
jztang <jztang8@gmail.com>
2021-07-08 15:37:34 -0400

    Create a factory function for tasks

---
+++ todo-list/src/task.js
@@ -0,0 +1,18 @@
+import { v4 as uuidv4 } from "uuid";
+
+/**
+ * Factory function for generating task objects.
+ * @param {string} name - The name of the task.
+ * @param {number} priority - The priority level of the task (1-4).
+ * @param {string} dueDate - The due date of the task.
+ * @param {string} project - The project this task belongs to.
+ * @returns 
+ */
+const taskFactory = (name, priority, dueDate, project) => {
+    const id = uuidv4();
+    return { id, name, priority, dueDate, project };
+};
+
+export {
+    taskFactory,
+}
