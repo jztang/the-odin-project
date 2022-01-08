25e58377e673c42b83b34d4d51a9bf27263c4c5d
jztang <jztang8@gmail.com>
2022-01-07 19:54:18 -0500

    Image slider practice

--- practice/image-slider/src/index.js
+++ practice/image-slider/src/index.js
@@ -1,16 +1,26 @@
 import { nextImage, prevImage, jumpToImage } from "./slider";
 
+let sliderTimer = setInterval(nextImage, 5000);
+
+function resetTimer() {
+  clearInterval(sliderTimer);
+  sliderTimer = setInterval(nextImage, 5000);
+}
+
 document.querySelector("#prev-image-btn").addEventListener("click", () => {
   prevImage();
+  resetTimer();
 });
 
 document.querySelector("#next-image-btn").addEventListener("click", () => {
   nextImage();
+  resetTimer();
 });
 
 const dots = document.querySelectorAll(".nav-dot");
 for (let i = 0; i < dots.length; i += 1) {
   dots[i].addEventListener("click", () => {
     jumpToImage(i);
+    resetTimer();
   });
 }
